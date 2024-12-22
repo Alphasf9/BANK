1a95a4ab1debede79762e562ff29bdc9c4910b4c
Aryan Kumar <aryankumar@Aryans-MacBook-Air.local>
2024-12-22 16:52:27 +0530

    models

---
+++ Backend/models/account.model.js
@@ -0,0 +1,56 @@
+import mongoose from "mongoose";
+
+const accountSchema = new mongoose.Schema({
+    accountHolder: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+
+    accountNumber: {
+        type: String,
+        unique: true,
+        required: true
+    },
+
+    accountType: {
+        type: String,
+        enum: ["Saving", "Current"],
+        required: true
+    },
+
+    branchDetails: {
+        branchName: {
+            type: String,
+            required: true,
+        },
+        branchCode: {
+            type: String,
+            required: true
+        },
+        ifscCode: {
+            type: String,
+            required: true
+        }
+    },
+
+    balance: {
+        type: Number,
+        required: true,
+        default: 0.0
+    },
+
+    nominee: {
+        name: {
+            type: String
+        },
+        relation: {
+            type: String
+        },
+        contact: {
+            type: String
+        }
+    }
+}, { timestamps: true });
+
+export const Account = mongoose.model("Account", accountSchema);
---
+++ Backend/models/transaction.model.js
@@ -0,0 +1,57 @@
+import mongoose from "mongoose";
+
+const transactionSchema = new mongoose.Schema({
+    transactionId: {
+        type: String,
+        unique: true,
+        required: true
+    },
+
+    accountNumber: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Account",
+        required: true
+    },
+
+    type: {
+        type: String,
+        enum: ["Credit", "Debit"],
+        required: true
+    },
+
+    amount: {
+        type: Number,
+        required: true,
+        validate: {
+            validator: function (value) {
+                return value > 0;
+            },
+            message: "Amount must be greater than zero."
+        }
+    },
+
+    transactionDate: {
+        type: Date,
+        default: Date.now,
+        required: true
+    },
+
+    //   description: {
+    //     type: String,
+    //     required: true // E.g., "ATM Withdrawal", "Online Transfer"
+    //   },
+
+    balanceAfterTransaction: {
+        type: Number,
+        required: true
+    },
+
+    status: {
+        type: String,
+        enum: ["Pending", "Completed", "Failed"],
+        default: "Completed",
+        required: true
+    }
+}, { timestamps: true });
+
+export const Transaction = mongoose.model("Transaction", transactionSchema);
