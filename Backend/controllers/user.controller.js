47d8f74cac1a8fbbf2b94f6ffbfecdfcb2e5e5a2
Alphasf9 <134485025+Alphasf9@users.noreply.github.com>
2024-12-24 17:12:25 +0530

    User Registartion done

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -5,25 +5,38 @@ import bcrypt from "bcrypt"
 
 const registerUser = async (req, res) => {
     try {
-
-        const { fullName, email, phoneNo, dob, gender, aadhar_id, address, maritalStatus, occupation, nationality, userPassword } = req.body;
-
-        console.log(req.body)
-        console.log("***");
-        
-        console.log(address);
-        
+        const {
+            firstName,
+            lastName,
+            email,
+            phoneNo,
+            dob,
+            gender,
+            aadhar_id,
+            street,
+            city,
+            state,
+            zip,
+            country,
+            maritalStatus,
+            occupation,
+            nationality,
+            userPassword
+        } = req.body;
 
         if (
-            !fullName ||
+            !firstName ||
+            !lastName ||
             !email ||
             !phoneNo ||
-            !address ||
+            !street ||
+            !city ||
+            !state ||
+            !zip ||
             !userPassword
         ) {
-            // return res.status(400).json({ message: "All fields are required." });
+            return res.status(400).json({ message: "All fields are required." });
         }
-
 
         const existedUser = await User.findOne({ aadhar_id });
         if (existedUser) {
@@ -32,59 +45,54 @@ const registerUser = async (req, res) => {
 
         const photoLocalPath = req.file?.path;
         console.log("photo path", photoLocalPath);
-        
+
         if (!photoLocalPath) {
             return res.status(400).json({ message: "Photo file is required." });
         }
 
         const photo = await uploadOnCloudinary(photoLocalPath);
         if (!photo) {
-            return res.status(400).json({ message: "Photo is required" })
+            return res.status(400).json({ message: "Photo is required." });
         }
 
         const hashedPassword = await bcrypt.hash(userPassword, 10);
 
-
         const user = await User.create({
-            firstName: fullName.firstName,
-            lastName: fullName.lastName,
+            fullName: { firstName, lastName },
             email,
             phoneNo,
             dob,
             gender,
             aadhar_id,
-            street: address.street,
-            state: address.state,
-            city: address.city,
-            zip: address.zip,
-            country: address.country,
+            address: {
+                street,
+                city,
+                state,
+                zip,
+                country: country || "INDIA",
+            },
             maritalStatus,
             occupation,
             nationality,
             photo: photo.url,
-            userPassword: hashedPassword
-        })
-        console.log(user);
-        
-        // await user.save();
+            userPassword: hashedPassword,
+        });
 
         const createdUser = await User.findById(user._id).select("-userPassword");
         if (!createdUser) {
-            return res.status(500).json({ message: "Somthing went worng while requesting user" })
+            return res.status(500).json({ message: "Something went wrong while requesting the user." });
         }
 
-        return res.status(201).json(
-            {
-                message: "User Register Successfully",
-                user: createdUser
-            }
-        )
+        return res.status(201).json({
+            message: "User registered successfully",
+            user: createdUser,
+        });
 
     } catch (error) {
         console.error("Error during user registration:", error);
         res.status(500).json({ message: "Internal server error." });
     }
+};
 
-}
 
 export { registerUser }
