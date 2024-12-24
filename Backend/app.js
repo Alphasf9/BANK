f8cb9423b0996ee60865af14e4daf5b5b842cd0f
Aryan Kumar <aryankumar@Aryans-MacBook-Air.local>
2024-12-24 13:20:13 +0530

    userController

--- Backend/app.js
+++ Backend/app.js
@@ -1,15 +1,27 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./db/db.js";
+import cookieParser from "cookie-parser";
+import userRouter from "./routes/user.route.js";
 
 const app = express();
 dotenv.config();
 
+//connection of DB
 connectDB();
-
-
 const port = process.env.PORT || 3000;
 
+// Settins for server and cookies
+app.use(express.json({limit: "16kb"}));
+app.use(express.urlencoded({extended: true, limit: "16kb"}));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+
+// Routes
+app.use("/api/v1/user", userRouter)
+
+// Server listining
 app.listen(port, ()=> {
     console.log(`Server is listen at port ${port}`);
 })
