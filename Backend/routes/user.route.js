87f9bda0e2fa2b5e77296b859880fab8baf3de6d
Alphasf9 <134485025+Alphasf9@users.noreply.github.com>
2024-12-29 20:07:35 +0530

    Add OTP verification functionality and session management - Implemented OTP sending via email using nodemailer. - Added routes for sending and verifying OTP. - Integrated session management with express-session and connect-mongo. - Updated user controller to handle OTP logic. - Enhanced package.json with new dependencies.

--- Backend/routes/user.route.js
+++ Backend/routes/user.route.js
@@ -1,7 +1,11 @@
 import { Router } from "express";
-import { blockUser, changePassword, getCurrentUser, loginUser, logoutUser, registerUser, updatePersonalDetails, updateUserPhoto } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+import {
+    blockUser, changePassword, checkOtpForVerification, getCurrentUser,
+    loginUser, logoutUser, registerUser,
+    sendOtp, updatePersonalDetails, updateUserPhoto
+} from "../controllers/user.controller.js";
 
 const router = Router();
 
@@ -14,5 +18,9 @@ router.put("/update-details", verifyJWT, updatePersonalDetails);
 router.post("/update-photo", verifyJWT, upload.single("photo"), updateUserPhoto);
 
 router.post("/block-user", blockUser);
+
+router.get('/getOtp', sendOtp)
+
+router.post('/verifyOtp', checkOtpForVerification)
 
 export default router;
